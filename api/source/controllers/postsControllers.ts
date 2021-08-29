8e402d0c554df6271a0d7438fcf8091abfaa6456
ademaus <48208485+amaurydelassus@users.noreply.github.com>
2021-08-29 21:12:32 +0200

    Api (#3)
    * add routes and controllers for events and parteners
    
    * event and partener by bdd.json

--- api/source/controllers/postsControllers.ts
+++ api/source/controllers/postsControllers.ts
@@ -1,19 +1,28 @@
 import { Request, Response, NextFunction } from 'express';
 import axios, { AxiosResponse } from 'axios';
 
-interface Post {
+interface Events {
     userId: Number;
     id: Number;
     title: String;
     body: String;
 }
 
-const getPosts = async (req: Request, res: Response, next: NextFunction) => {
-    let result: AxiosResponse = await axios.get('https://jsonplaceholder.typicode.com/posts');
-    let posts: [Post] = result.data;
-    return res.status(200).json({
-        message: posts
-    });
+interface Parteners {
+    userId: Number;
+    id: Number;
+    title: String;
+    body: String;
+}
+
+const getEvents = async (req: Request, res: Response, next: NextFunction) => {
+    let data = require('./bdd.json')
+    return res.status(200).json(data.events);
 };
 
-export default { getPosts };
+const getParteners = async (req: Request, res: Response, next: NextFunction) => {
+    let data = require('./bdd.json')
+    return res.status(200).json(data.parteners);
+};
+
+export default { getEvents , getParteners };
--- api/source/routes/postsRoutes.ts
+++ api/source/routes/postsRoutes.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import controller from '../controllers/postsControllers';
 const router = express.Router();
 
-router.get('/posts', controller.getPosts);
+router.get('/events', controller.getEvents);
+router.get('/parteners', controller.getParteners);
 
 export = router;
